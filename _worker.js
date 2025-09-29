63411f5ca962c8ee3a2a64e6e8bef363ff4c493d
yondor <yondor@outlook.com>
2025-09-29 10:40:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -258,6 +258,13 @@ export default {
 			});
 		}
 
+		return new Response(JSON.stringify(request.cf, null, 4), {
+        	status: 200,
+            headers: {
+				"Content-Type": "application/json;charset=utf-8",
+            },
+        });
+		
 		const url = new URL(req.url);
 		url.hostname = 'example.com';
 		return fetch(new Request(url, req));
